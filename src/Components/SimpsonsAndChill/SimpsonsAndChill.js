2937f298b21c2dead7cfc47752edc0ae77691e2d
julie1013 <julie1380@verizon.net>
2017-04-04 21:52:40 -0400

    Set up initial SimpsonsAndChill component
    Set up initial SimpsonsAndChill component, which the user will
    see upon logging in.

---
+++ src/Components/SimpsonsAndChill/SimpsonsAndChill.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './SimpsonsAndChill.css';
+
+export default class SimpsonsAndChill extends Component {
+  render() {
+    return(
+      <div className="simpsons-and-chill">
+      SIMPSONS AND CHILL!!
+      </div>
+    );
+  }
+}
