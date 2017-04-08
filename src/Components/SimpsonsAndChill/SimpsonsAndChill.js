93e31cbf53673e55fc3cfa9a242b0cf6e0fdf9a3
julie1013 <julie1380@verizon.net>
2017-04-08 16:37:15 -0400

    Practiced rendering titles from back end

--- src/Components/SimpsonsAndChill/SimpsonsAndChill.js
+++ src/Components/SimpsonsAndChill/SimpsonsAndChill.js
@@ -6,11 +6,25 @@ export default class SimpsonsAndChill extends Component {
   constructor () {
     super()
     mockSimpsonsBackend.init()
+    this.state={episodes: undefined}
+    this.componentDidMount = this.componentDidMount.bind(this);
   }
+
+  componentDidMount(){
+    mockSimpsonsBackend.getEpisodes()
+    .then((response)=>{
+      return response;
+    }).then((response)=>{
+      this.setState({episodes: response[2].title})
+    })
+  }
+
   render () {
     return (
       <div className='simpsons-and-chill'>
-      SIMPSONS AND CHILL!!
+        {this.state.episodes
+        ? <h1>{this.state.episodes}</h1>
+        : null }
       </div>
     )
   }
